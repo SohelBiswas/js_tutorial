5540a91e3e58a98c68c8e6bd74add46627d55ae2
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-01 05:34:45 +0000

    Datatypes

---
+++ 01/datatypes.js
@@ -0,0 +1,25 @@
+// let num = 21
+
+// console.log(num);
+
+
+// let name = "Sohel"
+// console.log(typeof name);
+
+
+let isLoggedIn = "Sohel"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggedIn);
+
+
+let score = 4
+++score 
+score++
+
+// console.log(score);
+
+
+console.log(null == 0);
+
