5044110a22b85efdcc72c8853659730f4d764403
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-04 05:09:19 +0000

    String

---
+++ 01/string.js
@@ -0,0 +1,36 @@
+const name = 'Sohel'
+const age = ' 26'
+
+// console.log(name + age);
+
+// console.log(`My name is ${name} and my age is ${age}`);
+
+
+const gameName = new String('sohel-sb-com')
+// const gameName = ('hitesh-hc-com')
+// console.log(gameName);
+
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf('h'));
+
+// console.log(gameName.length);
+
+// console.log(gameName.substring(0,7));
+
+
+const newString = gameName
+console.log(newString);
+
+
+console.log(newString.replace('sb', 'ad'));
+
+
+console.log(newString.includes('com'));
+
+
+
+
+
+
+
