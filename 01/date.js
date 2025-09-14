1f76d74f5a0b6dbdd694f00e817b3cfb65bfe36d
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-14 05:58:15 +0000

    Date conversion

---
+++ 01/date.js
@@ -0,0 +1,11 @@
+let date = new (Date)
+
+console.log(date.toLocaleString());
+console.log(date.toDateString());
+console.log(date.getMonth());
+console.log(date.getTime());
+console.log(date.getTime());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.getDay());
+
