4bf9a98791dc6b1dd208dca379c65c3184ede8e7
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-16 05:33:39 +0000

    Array Day 1

---
+++ 02/01_array.js
@@ -0,0 +1,27 @@
+const arr = [5,6,7,8,9]
+
+// console.log(arr);
+
+// arr.push(10)
+// arr.pop()
+// arr.unshift(4)
+// arr.shift()
+
+// console.log(arr);
+
+// console.log(arr.includes(100));
+
+// console.log(arr.indexOf(4));
+
+// const arr2 = [1,2,3,4]
+
+const arr2 = arr.join()
+
+// console.log(arr2);
+
+
+// console.log(arr.slice(1,4))
+//  console.log(arr);
+
+// console.log(arr.splice(1,4));
+    console.log(arr);
