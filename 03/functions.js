c592221db18295bed282fa6858f8c5fe3c91b70c
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-23 07:02:24 +0000

    Fynctions Message

--- 03/functions.js
+++ 03/functions.js
@@ -1,11 +1,46 @@
 function myname(){
 
-    console.log("S");
-    console.log("o");
-    console.log("h");
-    console.log("e");
-    console.log("l");
+    // console.log("S");
+    // console.log("o");
+    // console.log("h");
+    // console.log("e");
+    // console.log("l");
     
 }
 
-myname()
+// myname()
+
+// function addnum(num1, num2) {
+//     // console.log(num1 + num2);    
+// }
+// return addnum(3,4)
+
+
+// function loginuserMesage(username = "sam") {
+//     if(username ===""){
+//         console.log("Please enter a unsername");
+//         return
+        
+//     }
+//     return `${username} just logged in`
+// }
+// loginuserMesage()
+
+
+
+// function loginuserMesage(username = "sam") {
+//     return `${username} just logged in`;
+// }
+// loginuserMesage()
+
+
+
+
+function loginuserMesage(username = "sam") {
+    if (username === "") {
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username} just logged in`;
+}
+console.log(loginuserMesage());
