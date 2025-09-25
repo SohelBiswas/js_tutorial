9c34611f828e1d320f674d9a95434a83915c36d0
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-25 04:48:56 +0000

    Scope intro

---
+++ 03/scope.js
@@ -0,0 +1,14 @@
+function one() {
+    const username = 'Hulk'
+    
+    // function two(){
+    //     const originalname = "Bruce Banner" 
+    // }
+    // two()
+    // // console.log(username);
+    return one()
+}
+console.log(username);
+
+
+
