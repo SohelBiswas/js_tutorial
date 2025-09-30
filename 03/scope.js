2f0ea9326edc2ccf94e9074639b4bfe441e90d4c
Abu Sohel Biswas <88239407+SohelBiswas@users.noreply.github.com>
2025-09-30 04:09:59 +0000

    Scope day 2

--- 03/scope.js
+++ 03/scope.js
@@ -1,14 +1,23 @@
 function one() {
     const username = 'Hulk'
     
-    // function two(){
-    //     const originalname = "Bruce Banner" 
-    // }
-    // two()
-    // // console.log(username);
-    return one()
+    function two(){
+        const originalname = "Bruce Banner" 
+
+        console.log(originalname);
+        
+        }
+
+    two()
+    // console.log(username);
+    // return one()
 }
-console.log(username);
+// console.log(username);
+// one()
 
 
-
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+chai()
