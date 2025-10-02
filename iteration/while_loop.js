5501d4f83bf6b67413bfa44dced53ef9acf70f02
= <mrsohel.biswas@gmail.com>
2025-10-02 10:11:49 +0530

    While looop with Array

---
+++ iteration/while_loop.js
@@ -0,0 +1,10 @@
+let myarr = ['Iron Man', 'Thor', 'Hulk', 'Ant Man']
+
+console.log(`The length of the array is ${myarr.length}`);
+
+let index = 0
+while (index < myarr.length) {
+        console.log(`The Avenger is ${myarr[index]}`);
+    index = index += 1;
+    
+}
