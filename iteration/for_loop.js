b1a62b7b38cea655491f3e9764e731ef3edd4cfa
= <mrsohel.biswas@gmail.com>
2025-10-02 10:11:15 +0530

    For loop with array

---
+++ iteration/for_loop.js
@@ -0,0 +1,38 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+
+    for (let j = 0; j < 10; j++) {
+        const element = j;
+        // console.log('Sohel');
+        
+    }
+        
+    // console.log(i);
+    
+}
+
+
+for (let i = 0; i < 7; i++) {
+    const element = i;
+    
+    // console.log(i);
+    if (i == 5) {
+        // console.log('5 detected');
+        // break
+        continue
+    }
+    // console.log(`Value of i is ${i}`);
+}
+
+
+let myarray = ['Ironman', 'Captain America', 'Thor','Hulk']
+console.log(myarray.length);
+
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index]
+
+    console.log(`The Avengers are ${myarray[index]}`);
+    
+}
+
+
