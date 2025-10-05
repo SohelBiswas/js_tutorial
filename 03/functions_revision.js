9ab89569995688fde7c7733ce8bcc8a9ada86fd8
= <mrsohel.biswas@gmail.com>
2025-10-05 10:24:30 +0530

    Functions and if dive

--- 03/functions_revision.js
+++ 03/functions_revision.js
@@ -6,6 +6,9 @@ function login(username) {
 // console.log(login("Sohel"))
 
 
+// **********Test 1**************
+// ******************************
+
 function addnum(num1, num2) {
     if (typeof num1 == 'number' && typeof num2 == 'number') {
         let result = num1 + num2
@@ -17,4 +20,22 @@ function addnum(num1, num2) {
 }
 
 // addnum(3, 5)
+// console.log(addnum(3, 5));
+
+
+
+// **********Test 2**************
+// ******************************
+
+function addnum(num1, num2) {
+    if (typeof num1 == 'number' && typeof num2 == 'number') {
+        let result = num1 + num2
+        return result
+    }    
+    // return console.log("Not a number");
+    
+   return "Not a number";
+  
+}
+
 console.log(addnum(3, 5));
