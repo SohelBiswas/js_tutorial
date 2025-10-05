3d6840e3fa3a60d6c3f4b74b54706b4b41b478f1
= <mrsohel.biswas@gmail.com>
2025-10-05 10:11:34 +0530

    While loop basic

--- iteration/while_loop.js
+++ iteration/while_loop.js
@@ -1,10 +1,15 @@
 let myarr = ['Iron Man', 'Thor', 'Hulk', 'Ant Man']
 
-console.log(`The length of the array is ${myarr.length}`);
+// console.log(`The length of the array is ${myarr.length}`);
 
 let index = 0
 while (index < myarr.length) {
-        console.log(`The Avenger is ${myarr[index]}`);
+        // console.log(`The Avenger is ${myarr[index]}`);
     index = index += 1;
     
 }
+
+
+
+let i = null;
+console.log(typeof i);
