45f8f1aeef88839c1e707a89ac39276c6633a9cf
= <mrsohel.biswas@gmail.com>
2025-10-06 08:49:14 +0530

    Function revision with object

---
+++ 03/function_revision1.js
@@ -0,0 +1,26 @@
+// const avenger = {
+//     name: "Tony Stark",
+//     Alias: "Iron Man",
+//     serialno: 1
+// }
+
+// function handleobject() {
+//     console.log(`The name of the Avenger is ${avenger.name}, alias is ${avenger.Alias} and serial is ${avenger.serialno}`);
+    
+// }
+
+//  handleobject();
+
+
+ const avenger = {
+    name: "Tony Stark",
+    Alias: "Iron Man",
+    serialno: 1
+}
+
+function handleobject(marvel) {
+    console.log(`The name of the Avenger is ${marvel.name}, alias is ${marvel.Alias} and serial is ${marvel.serialno}`);
+    
+}
+
+ handleobject(avenger);
