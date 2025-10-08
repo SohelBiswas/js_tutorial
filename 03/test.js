8da893d6e304273fecf79a55c8af91a01287ff73
= <mrsohel.biswas@gmail.com>
2025-10-08 14:57:42 +0530

    func + if with ternary operator

--- 03/test.js
+++ 03/test.js
@@ -6,5 +6,17 @@ function gymbro (gym) {
         
     }
 }
-gymbro("Ani")
+// gymbro("Ani")
 
+
+
+// const gymbro = "Anii"
+
+// const gym = (gymbro)
+
+function gymbro(gym) {
+    let workout = (gym == "Ani") ? "Gym kokhon jbi?" : "Bhag";
+    console.log(workout);
+    
+}
+gymbro("Ani")
