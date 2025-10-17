db01860301b8c1818760c36c8194853d07cabded
= <mrsohel.biswas@gmail.com>
2025-10-17 08:13:35 +0530

    For-each iteration

--- iteration/for_each.js
+++ iteration/for_each.js
@@ -1,7 +1,53 @@
 const marvel = ["Iron Man", "Thor", "Hulk", "Captain America"]
 
-marvel.forEach(function avenger (heros) {
-    console.log(heros);
-    console.log(avenger);
+marvel.forEach (function avenger (heros) {
+    // console.log(heros);
+    // console.log(avenger);
     
 })
+
+
+const students = [
+  {
+    name: "Alice",
+    age: 20,
+    major: "Computer Science"
+  },
+  {
+    name: "Bob",
+    age: 22,
+    major: "Electrical Engineering"
+  },
+  {
+    name: "Charlie",
+    age: 21,
+    major: "Mathematics"
+  },
+  {
+    name: "Max",
+    age: 22,
+    major: "Computer Science"
+  },
+  {
+    name: "Tom",
+    age: 21,
+    major: "Electrical Engineering"
+  },
+];
+
+const StudentAge = students.filter( (stud) => stud.age === 21)
+console.log(StudentAge);
+
+
+students.forEach( (stud) => {
+    if (stud.age === 21) {
+        // console.log(stud);
+        return stud
+    }
+})
+
+
+
+
+
+
