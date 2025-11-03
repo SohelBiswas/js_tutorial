bd2760f9ceb94c90cd0699e796be662ce82b1ab7
= <mrsohel.biswas@gmail.com>
2025-11-03 11:19:44 +0530

    Class intro

---
+++ 09_oops/class.js
@@ -0,0 +1,16 @@
+class user {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`    
+    }
+}
+
+const thor = new user('Thor', 'thor@avengers.com', '1234')
+
+console.log(thor);
+console.log(thor.encryptPassword());
