1fadbeb61caf157175a6d2e32a8540623f1f941f
= <mrsohel.biswas@gmail.com>
2025-11-10 12:39:11 +0530

    Update code for JS learning

---
+++ experiment/loop.js
@@ -0,0 +1,36 @@
+// let teaCollection = []
+// let tea
+
+// do {
+//     // tea = prompt("Enter tea types")
+//     // if (tea != "stop") {
+//         // teaCollection.push(tea)
+//     // }
+// // } while (tea != "stop");
+
+// // console.log(teaCollection);
+
+
+
+// let total = 0
+// let i = 0
+// do { 
+//     total = total + i
+//     i++
+// } while (i<=3);
+
+// // console.log(total);
+
+
+
+
+let multipliedNumber = []
+let number = [2,4,6]
+
+for (let k = 0; k < number.length; k++) {
+    // let takeNumber = number[k] * 2
+    // multipliedNumber.push(takeNumber
+    multipliedNumber.push(number[k]*2)
+}
+console.log(multipliedNumber);
+
