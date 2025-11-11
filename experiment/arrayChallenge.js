3d68c9cad4d928686903aa4f7c1dc5bfd6ca6d16
= <mrsohel.biswas@gmail.com>
2025-11-11 10:22:25 +0530

    Deep dive JS learning with loops

--- experiment/arrayChallenge.js
+++ experiment/arrayChallenge.js
@@ -1,9 +1,24 @@
+// let teas = ['green tea', 'black tea', 'chai', 'oolong tea']
+// let selectedteas = []
+
+// for (let i = 0; i < teas.length; i++) {
+//     if (teas[i] === 'chai') {
+//         break;
+//     }
+//         selectedteas.push(teas[i])    
+// }
+//         console.log(selectedteas);
+
+
+
+
+
 let teas = ['green tea', 'black tea', 'chai', 'oolong tea']
 let selectedteas = []
 
 for (let i = 0; i < teas.length; i++) {
     if (teas[i] === 'chai') {
-        break;
+        continue;
     }
         selectedteas.push(teas[i])    
 }
@@ -11,3 +26,40 @@ for (let i = 0; i < teas.length; i++) {
 
 
         
+
+
+let numbers = [1,2,3,4,5]
+let smallNumbers = []
+
+for (const num of numbers) {
+    if (num === 4) {
+        break;
+    }
+    smallNumbers.push(num)
+}
+console.log(smallNumbers);
+
+
+
+
+let citiesPopulation = {
+    'london': 2000,
+    'New York': 3000,
+    'Paris': 4000,
+    'Berlin': 5000,
+};
+let citiesNewPopulation = {}
+
+for (const city in citiesPopulation) {
+
+    if (city =='berlin' || city == 'Berlin') {
+        break;
+    }
+
+    citiesNewPopulation[city] = citiesPopulation[city]
+    // console.log(citiesNewPopulation);
+}
+
+console.log(citiesNewPopulation);
+
+
