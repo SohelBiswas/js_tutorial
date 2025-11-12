ce4cf10cad87028a462f21924c5a2c57bf603adb
= <mrsohel.biswas@gmail.com>
2025-11-12 11:04:06 +0530

    New code with Higher order function

--- experiment/functionChallenge.js
+++ experiment/functionChallenge.js
@@ -6,5 +6,38 @@ function orderTea (teaType) {
 }
 // console.log(orderTea())
 // orderTea('chai')
-console.log(orderTea('chai'));
-console.log(orderTea('masala chai'));
+// console.log(orderTea('chai'));
+// console.log(orderTea('masala chai'));
+
+
+
+
+
+const calculateTotal = (price , quantity) => {
+    return price * quantity;
+}
+
+let totalCost = calculateTotal(100, 2)
+// console.log(totalCost);
+
+// console.log(calculateTotal(5,2));
+
+
+
+
+
+function processTeaOrder(makeTea) {
+    return makeTea('earl grey')
+}
+
+function makeTea(teaType) {
+    return `Ordered tea is ${teaType}`
+}
+
+// const orderedTea = processTeaOrder(makeTea)
+// console.log(orderedTea);
+console.log(processTeaOrder(makeTea));
+
+
+
+
